6972b19867dbfe76db264d081910844f9cf71152
czichy <christian@czichy.com>
2024-07-05 16:35:20 +0200

    microvm

--- parts/modules/nixos/services/virtualisation/microvm/test.nix
+++ parts/modules/nixos/services/virtualisation/microvm/test.nix
@@ -69,7 +69,7 @@ in {
         };
 
         config = {
-          services_networking_ssh = importApply ../networking/ssh.nix {inherit localFlake;};
+          services_networking_ssh = importApply ../../networking/ssh.nix {inherit localFlake;};
 
           microvm = {
             # Any other configuration for your MicroVM
