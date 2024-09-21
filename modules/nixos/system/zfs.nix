2beecb26148d8e4e806159df90f6979d835df7e8
czichy <christian@czichy.com>
2024-09-21 18:16:04 +0200

    zfs

--- modules/nixos/system/zfs.nix
+++ modules/nixos/system/zfs.nix
@@ -163,18 +163,18 @@ in {
     # |----------------------------------------------------------------------| #
     (lib.mkIf (cfg.zfs.root.impermanenceRoot) {
       # TODO remove once this is upstreamed
-      boot.initrd.systemd.services."zfs-import-rpool".after = ["cryptsetup.target"];
+      # boot.initrd.systemd.services."zfs-import-rpool".after = ["cryptsetup.target"];
       # After importing the rpool, rollback the root system to be empty.
-      boot.initrd.systemd.services.impermanence-root = {
-        wantedBy = ["initrd.target"];
-        after = ["zfs-import-rpool.service"];
-        before = ["sysroot.mount"];
-        unitConfig.DefaultDependencies = "no";
-        serviceConfig = {
-          Type = "oneshot";
-          ExecStart = "${pkgs.zfs}/bin/zfs rollback -r zroot/local/root@blank";
-        };
-      };
+      # boot.initrd.systemd.services.impermanence-root = {
+      #   wantedBy = ["initrd.target"];
+      #   after = ["zfs-import-rpool.service"];
+      #   before = ["sysroot.mount"];
+      #   unitConfig.DefaultDependencies = "no";
+      #   serviceConfig = {
+      #     Type = "oneshot";
+      #     ExecStart = "${pkgs.zfs}/bin/zfs rollback -r zroot/local/root@blank";
+      #   };
+      # };
       #   boot.initrd.postDeviceCommands =
       #     #wipe / and /var on boot
       #     lib.mkAfter ''
