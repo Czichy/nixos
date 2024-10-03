e2e10c3f6c452dcaeb527093ab777c88d2a54650
czichy <christian@czichy.com>
2024-10-03 17:16:20 +0200

    remove proxy-public

--- hosts/HL-1-MRZ-SBC-01-opnsense/net.nix
+++ hosts/HL-1-MRZ-SBC-01-opnsense/net.nix
@@ -11,7 +11,7 @@ in {
   wireguard.proxy-vps.server = {
     host = config.networking.fqdn;
     port = 51820;
-    reservedAddresses = ["10.46.0.0/24" "fd00:43::/120"];
+    reservedAddresses = ["10.46.0.1/24" "fd00:43::/120"];
     openFirewall = true;
   };
 }
