220030a41eac6951ce222619930226aa8e7446b0
czichy <christian@czichy.com>
2024-10-05 00:47:29 +0200

    remove proxy-public

--- hosts/HL-1-MRZ-SBC-01-opnsense/net.nix
+++ hosts/HL-1-MRZ-SBC-01-opnsense/net.nix
@@ -8,16 +8,16 @@
   inherit (inputs.self) lib;
 in {
   networking.domain = globals.domains.me;
-  wireguard.proxy-vps = {
-    ipv4 = "10.46.0.1";
-    server = {
-      host = config.networking.fqdn;
-      port = 51820;
-      reservedAddresses = [
-        "10.46.0.1/24"
-        "fd00:43::/120"
-      ];
-      openFirewall = true;
-    };
-  };
+  # wireguard.proxy-vps = {
+  #   ipv4 = "10.46.0.1";
+  #   server = {
+  #     host = config.networking.fqdn;
+  #     port = 51820;
+  #     reservedAddresses = [
+  #       "10.46.0.1/24"
+  #       "fd00:43::/120"
+  #     ];
+  #     openFirewall = true;
+  #   };
+  # };
 }
