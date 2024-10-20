4f31a029f47bad43e9c9a8126290ba0f7e6ff05e
czichy <christian@czichy.com>
2024-10-20 23:39:20 +0200

    wireguard proxy

--- hosts/HL-4-PAZ-PROXY-01/net.nix
+++ hosts/HL-4-PAZ-PROXY-01/net.nix
@@ -122,12 +122,12 @@ in {
             #into the already existing public-key cryptography, for post-quantum resistance.
             PresharedKeyFile = config.age.secrets.preshared-key.path;
             # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
-            AllowedIPs = ["10.46.0.1" "10.15.70.1"];
+            AllowedIPs = ["10.46.0.1/32" "10.15.70.1/32"];
             # It represents, in seconds, between 1 and 65535 inclusive,
             # how often to send an authenticated empty packet to the peer,
             # for the purpose of keeping a stateful firewall or NAT mapping valid persistently.
             PersistentKeepalive = 25;
-            Endpoint = "92.116.142.216:51820";
+            # Endpoint = "92.116.142.216:51820";
           }
         ];
       };
