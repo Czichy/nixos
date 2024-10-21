1986e42b07b71e16803a424ab55ae7fb4f2909a9
czichy <christian@czichy.com>
2024-10-21 20:41:22 +0200

    uptime-kuma

--- modules/nixos/services/uptime-kuma.nix
+++ modules/nixos/services/uptime-kuma.nix
@@ -14,6 +14,7 @@ with lib; let
     (localFlake.lib)
     mkImpermanenceEnableOption
     isModuleLoadedAndEnabled
+    mkAgenixEnableOption
     ;
 
   cfg = config.tensorfiles.services.uptime-kuma;
