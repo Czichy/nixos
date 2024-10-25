94e74ad2d0db9b902611f6cafc5909d22c37ca8b
czichy <christian@czichy.com>
2024-10-25 09:33:31 +0200

    host zfs

--- hosts/HL-1-MRZ-HOST-01/disko.nix
+++ hosts/HL-1-MRZ-HOST-01/disko.nix
@@ -134,5 +134,5 @@ in {
   # Needed for impermanence, because we mount /persist/save on /persist, we need to make sure /persist is mounted before /persist/save
   fileSystems."/persist".neededForBoot = true;
   fileSystems."/state".neededForBoot = true;
-  boot.initrd.systemd.services."zfs-import-storage".after = ["cryptsetup.target" "zfs-import-rpool"];
+  boot.initrd.systemd.services."zfs-import-storage".after = ["cryptsetup.target"];
 }
