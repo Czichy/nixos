431404a3464b527a476fcbc8faa49c7848b24183
czichy <christian@czichy.com>
2024-11-02 10:30:41 +0100

    samba

--- hosts/HL-1-MRZ-HOST-01/guests/samba.nix
+++ hosts/HL-1-MRZ-HOST-01/guests/samba.nix
@@ -686,7 +686,7 @@ in {
       repository = "rclone:onedrive_nas:/backup/private";
 
       # Which local paths to backup, in addition to ones specified via `dynamicFilesFrom`.
-      paths = [config.services.vaultwarden.backupDir];
+      paths = ["/shares/users"];
 
       # Patterns to exclude when backing up. See
       #   https://restic.readthedocs.io/en/latest/040_backup.html#excluding-files
