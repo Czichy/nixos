ddcc722959857417ffc519eb5cc1e679f83a4170
czichy <christian@czichy.com>
2024-11-09 23:10:28 +0100

    host-02

--- modules/nixos/profiles/server.nix
+++ modules/nixos/profiles/server.nix
@@ -1,17 +1,3 @@
-# --- parts/modules/nixos/profiles/graphical-hyprland.nix
-#
-# Author:  czichy <christian@czichy.com>
-# URL:     https://github.com/czichy/tensorfiles
-# License: MIT
-#
-# 888                                                .d888 d8b 888
-# 888                                               d88P"  Y8P 888
-# 888                                               888        888
-# 888888 .d88b.  88888b.  .d8888b   .d88b.  888d888 888888 888 888  .d88b.  .d8888b
-# 888   d8P  Y8b 888 "88b 88K      d88""88b 888P"   888    888 888 d8P  Y8b 88K
-# 888   88888888 888  888 "Y8888b. 888  888 888     888    888 888 88888888 "Y8888b.
-# Y88b. Y8b.     888  888      X88 Y88..88P 888     888    888 888 Y8b.          X88
-#  "Y888 "Y8888  888  888  88888P'  "Y88P"  888     888    888 888  "Y8888   88888P'
 {localFlake}: {
   config,
   lib,
@@ -39,6 +25,11 @@ in {
         services.x11.desktop-managers.startx-home-manager.enable = _ true;
       };
 
+      xdg.icons.enable = _ false;
+      xdg.mime.enable = _ false;
+      xdg.sounds.enable = _ false;
+
+      documentation.man.enable = _ false;
       # environment.systemPackages = with pkgs; [
       # -- GENERAL PACKAGES --
       #libnotify # A library that sends desktop notifications to a notification daemon
