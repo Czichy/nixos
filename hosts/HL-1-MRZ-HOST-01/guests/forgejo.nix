ffffa0a98aab376e76a3bbf29ed1369e56d276c8
czichy <christian@czichy.com>
2024-11-17 13:35:43 +0100

    forgejo

--- hosts/HL-1-MRZ-HOST-01/guests/forgejo.nix
+++ hosts/HL-1-MRZ-HOST-01/guests/forgejo.nix
@@ -130,9 +130,9 @@ in {
   };
 
   services.openssh = {
-    # authorizedKeysFiles = lib.mkForce [
-    #   "${config.services.forgejo.stateDir}/.ssh/authorized_keys"
-    # ];
+    authorizedKeysFiles = lib.mkForce [
+      "${config.services.forgejo.stateDir}/.ssh/authorized_keys"
+    ];
     # Recommended by forgejo: https://forgejo.org/docs/latest/admin/recommendations/#git-over-ssh
     settings.AcceptEnv = "GIT_PROTOCOL";
   };
