7cff4d4716f967539c70ece3b0df2f060037b6f5
czichy <christian@czichy.com>
2024-11-28 13:37:00 +0100

    restructuring hosts

--- hosts/HL-1-MRZ-HOST-01/guests.nix
+++ hosts/HL-1-MRZ-HOST-01/guests.nix
@@ -95,9 +95,9 @@
       // mkMicrovm "syncthing" "HL-3-RZ-SYNC-01" "enp4s0" "vlan40" {
         enableSharedDataset = true;
       }
-      // mkMicrovm "forgejo" "HL-3-RZ-GIT-01" "enp4s0" "vlan40" {
-        enableStorageDataset = true;
-      }
+      # // mkMicrovm "forgejo" "HL-3-RZ-GIT-01" "enp4s0" "vlan40" {
+      #   enableStorageDataset = true;
+      # }
       // mkMicrovm "ibkr-flex" "HL-3-RZ-IBKR-01" "enp4s0" "vlan40" {
       }
     );
