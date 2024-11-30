f5b8984b09e9443a4378cb0061e0842aae68a9f8
czichy <christian@czichy.com>
2024-11-30 15:46:17 +0100

    ibkr-flex

--- parts/pkgs/ibkr-rust.nix
+++ parts/pkgs/ibkr-rust.nix
@@ -4,6 +4,12 @@
   fetchFromGitHub,
   pkg-config,
   openssl,
+  # stdenv,
+  # mandown,
+  # installShellFiles,
+  # curl,
+  # versionCheckHook,
+  # nix-update-script,
   ...
 }:
 rustPlatform.buildRustPackage rec {
@@ -17,17 +23,66 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-e65QDbK55q1Pbv/i7bDYRY78jgEUD1q6TLdKD8Gkswk=";
   };
 
+  env.OPENSSL_NO_VENDOR = 1;
+
   nativeBuildInputs = [pkg-config];
 
   buildInputs = [openssl];
 
   # NOTE needed due to Cargo.lock containing git dependencies
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "ibkr-rust-0.1.0" = "sha256-fCjVfmjrwMSa8MFgnC8n5jPzdaqSmNNdMRaYHNbs8Bo=";
-    };
-  };
+  # cargoLock = {
+  #   lockFile = ./Cargo.lock;
+  #   outputHashes = {
+  #     "ibkr-rust-0.1.0" = "sha256-fCjVfmjrwMSa8MFgnC8n5jPzdaqSmNNdMRaYHNbs8Bo=";
+  #   };
+  # };
+
+  # preCheck = ''
+  #   HOME=$(mktemp -d)
+  # '';
+
+  # nativeInstallCheckInputs = [
+  #   versionCheckHook
+  # ];
+  # versionCheckProgramArg = [ "--version" ];
+  # doInstallCheck = true;
+
+  # # Ensure that we don't vendor curl, but instead link against the libcurl from nixpkgs
+  # installCheckPhase = lib.optionalString (stdenv.hostPlatform.libc == "glibc") ''
+  #   runHook preInstallCheck
+
+  #   ldd "$out/bin/zellij" | grep libcurl.so
+
+  #   runHook postInstallCheck
+  # '';
+
+  # postInstall =
+  #   ''
+  #     mandown docs/MANPAGE.md > zellij.1
+  #     installManPage zellij.1
+  #   ''
+  #   + lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
+  #     installShellCompletion --cmd $pname \
+  #       --bash <($out/bin/zellij setup --generate-completion bash) \
+  #       --fish <($out/bin/zellij setup --generate-completion fish) \
+  #       --zsh <($out/bin/zellij setup --generate-completion zsh)
+  #   '';
+
+  # passthru.updateScript = nix-update-script { };
+
+  # meta = {
+  #   description = "Terminal workspace with batteries included";
+  #   homepage = "https://zellij.dev/";
+  #   changelog = "https://github.com/zellij-org/zellij/blob/v${version}/CHANGELOG.md";
+  #   license = with lib.licenses; [ mit ];
+  #   maintainers = with lib.maintainers; [
+  #     therealansh
+  #     _0x4A6F
+  #     abbe
+  #     pyrox0
+  #   ];
+  #   mainProgram = "zellij";
+  # };
 
   meta = with lib; {
     description = "";
