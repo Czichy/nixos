03b1fa22454c7e5d6d2d6eb1ee72172fee1e3a96
czichy <christian@czichy.com>
2024-12-05 19:30:15 +0100

    power-meter

--- hosts/HL-1-MRZ-HOST-01/hardware-configuration.nix
+++ hosts/HL-1-MRZ-HOST-01/hardware-configuration.nix
@@ -70,16 +70,16 @@
       };
     };
     # Newest kernels might not be supported by ZFS
-    kernelPackages = pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_6.override {
-      argsOverride = rec {
-        src = pkgs.fetchurl {
-          url = "mirror://kernel/linux/kernel/v${lib.versions.major version}.x/linux-${version}.tar.xz";
-          sha256 = "sha256-VeW8vGjWZ3b8RolikfCiSES+tXgXNFqFTWXj0FX6Qj4=";
-        };
-        version = "6.10.14";
-        modDirVersion = "6.10.14";
-      };
-    });
+    # kernelPackages = pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_6.override {
+    #   argsOverride = rec {
+    #     src = pkgs.fetchurl {
+    #       url = "mirror://kernel/linux/kernel/v${lib.versions.major version}.x/linux-${version}.tar.xz";
+    #       sha256 = "sha256-VeW8vGjWZ3b8RolikfCiSES+tXgXNFqFTWXj0FX6Qj4=";
+    #     };
+    #     version = "6.10.14";
+    #     modDirVersion = "6.10.14";
+    #   };
+    # });
     # kernelPackages = pkgs.linuxPackages_latest;
   };
 
