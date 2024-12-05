2067da2f4fb512ebd220edfa61fcd56240338135
czichy <christian@czichy.com>
2024-12-05 19:24:51 +0100

    power-meter

--- hosts/HL-1-MRZ-HOST-01/hardware-configuration.nix
+++ hosts/HL-1-MRZ-HOST-01/hardware-configuration.nix
@@ -69,7 +69,18 @@
         configurationLimit = 8;
       };
     };
-    kernelPackages = pkgs.linuxPackages_latest;
+    # Newest kernels might not be supported by ZFS
+    kernelPackages = lib.mkForce pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_6.override {
+      argsOverride = rec {
+        src = pkgs.fetchurl {
+          url = "mirror://kernel/linux/kernel/v${lib.versions.major version}.x/linux-${version}.tar.xz";
+          sha256 = "sha256-VeW8vGjWZ3b8RolikfCiSES+tXgXNFqFTWXj0FX6Qj4=";
+        };
+        version = "6.10.14";
+        modDirVersion = "6.10.14";
+      };
+    });
+    # kernelPackages = pkgs.linuxPackages_latest;
   };
 
   hardware = {
