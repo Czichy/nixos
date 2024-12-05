f292cad833a8f92e26c65e879e728d3bdeb6ce76
czichy <christian@czichy.com>
2024-12-05 15:52:26 +0100

    power-meter

--- hosts/HL-1-MRZ-HOST-01/guests/power-meter.nix
+++ hosts/HL-1-MRZ-HOST-01/guests/power-meter.nix
@@ -1,14 +1,37 @@
 {
   inputs,
+  config,
   pkgs,
+  secretsPath,
   hostName,
   ...
-}:
-# let
-# |----------------------------------------------------------------------| #
-# |----------------------------------------------------------------------| #
-# in
-{
+}: let
+  # |----------------------------------------------------------------------| #
+  query = "639991";
+  slug = "https://health.czichy.com/ping/";
+  download-ibkr-flex =
+    pkgs.writeShellScriptBin "ibkr-flex-download"
+    ''
+      #!/usr/bin/env bash
+      set -euo pipefail
+
+      echo "Downloading Flex Report"
+      token="$(cat ${config.age.secrets.ibkrFlexToken.path})";
+      /run/current-system/sw/bin/ibkr-rust-flex -q ${query} -t "$token" --dump-path /TWS_Flex_Reports
+
+      for file in /TWS_Flex_Reports/*.xml ; do
+          fileDate=$(${pkgs.gawk}/bin/awk -F[_.] '{print $3 }' <<<"$(basename "$file")");
+          destination="$(${pkgs.gawk}/bin/awk -F[-] '{print $1 }' <<<"$fileDate")/$(${pkgs.gawk}/bin/awk -F[-] '{print $1"-"$2  }' <<<"$fileDate")/"
+          echo "$destination"
+          mkdir -p "/TWS_Flex_Reports/$destination";
+          mv "$file" "/TWS_Flex_Reports/$destination";
+      done
+      pingKey="$(cat ${config.age.secrets.ibkr-flex-hc-ping.path})";
+      ${pkgs.curl}/bin/curl -m 10 --retry 5 --retry-connrefused "${slug}$pingKey/ibkr-flex-download"
+
+    '';
+  # |----------------------------------------------------------------------| #
+in {
   microvm.mem = 512;
   microvm.vcpu = 1;
   microvm.shares = [
@@ -26,50 +49,52 @@
 
   # |----------------------------------------------------------------------| #
   users = {
-    users.power = {
+    users.ibkr = {
       isSystemUser = true;
-      group = "power";
+      group = "ibkr";
     };
-    groups.power = {};
+    groups.ibkr = {};
   };
   # |----------------------------------------------------------------------| #
-  # age.secrets = {
-  #   ibkrFlexToken = {
-  #     symlink = true;
-  #     file = secretsPath + "/hosts/HL-1-MRZ-HOST-01/guests/ibkr-flex/token.age";
-  #     mode = "0600";
-  #     owner = "root";
-  #   };
-  # };
-  # age.secrets.ibkr-flex-hc-ping = {
-  #   file = secretsPath + "/hosts/HL-4-PAZ-PROXY-01/healthchecks-ping.age";
-  #   mode = "440";
-  #   owner = "root";
-  # };
+  age.secrets = {
+    ibkrFlexToken = {
+      symlink = true;
+      file = secretsPath + "/hosts/HL-1-MRZ-HOST-01/guests/ibkr-flex/token.age";
+      mode = "0600";
+      owner = "root";
+    };
+  };
+  age.secrets.ibkr-flex-hc-ping = {
+    file = secretsPath + "/hosts/HL-4-PAZ-PROXY-01/healthchecks-ping.age";
+    mode = "440";
+    owner = "root";
+  };
   # ------------------------------
   # | SYSTEM PACKAGES |
   # ------------------------------
   environment.systemPackages = with pkgs; [
+    gawk
     pkg-config
-    inputs.power-meter.packages.${pkgs.system}.power-meter
+    openssh
+    inputs.ibkr-rust.packages.${pkgs.system}.flex
   ];
   # |----------------------------------------------------------------------| #
-  # systemd.timers."ibkr-flex-download" = {
-  #   wantedBy = ["timers.target"];
-  #   timerConfig = {
-  #     Perisistent = true;
-  #     OnCalendar = "Mon..Fri 23:30";
-  #     Unit = "ibkr-flex-download.service";
-  #   };
-  # };
+  systemd.timers."ibkr-flex-download" = {
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      Perisistent = true;
+      OnCalendar = "Mon..Fri 23:30";
+      Unit = "ibkr-flex-download.service";
+    };
+  };
 
-  # systemd.services."ibkr-flex-download" = {
-  #   serviceConfig = {
-  #     Type = "simple";
-  #     User = "root";
-  #     ExecStart = "${download-ibkr-flex}/bin/ibkr-flex-download";
-  #   };
-  # };
+  systemd.services."ibkr-flex-download" = {
+    serviceConfig = {
+      Type = "simple";
+      User = "root";
+      ExecStart = "${download-ibkr-flex}/bin/ibkr-flex-download";
+    };
+  };
 
   # |----------------------------------------------------------------------| #
   environment.persistence."/persist".files = [
