cb083473a1d1502f8a6274c1af4206373b76c29e
czichy <christian@czichy.com>
2024-12-06 12:25:50 +0100

    power-meter

--- globals.nix
+++ globals.nix
@@ -60,10 +60,8 @@ in {
         hosts.HL-3-RZ-GIT-01.id = 14;
         # IBKR Flex Downloader
         hosts.HL-3-RZ-IBKR-01.id = 15;
-        # IBKR Flex Downloader
-        hosts.HL-3-RZ-IBKR-02.id = 16;
         # Power Meter
-        hosts.HL-3-RZ-POWER-01.id = 32;
+        hosts.HL-3-RZ-POWER-01.id = 17;
         # |------------------------------------| #
         hosts.HL-1-MRZ-HOST-02.id = 20;
         # AdguardHome
