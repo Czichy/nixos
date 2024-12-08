f9b88cca978e20acba184f37cb1718a662e5be60
czichy <christian@czichy.com>
2024-12-08 11:36:15 +0100

    power-meter

--- hosts/HL-1-MRZ-HOST-01/guests/powermeter.nix
+++ hosts/HL-1-MRZ-HOST-01/guests/powermeter.nix
@@ -15,25 +15,15 @@
     {
       # Lesekopf - Silicon_Labs_CP2104_USB_to_UART_Bridge_Controller_015ACA59
       bus = "usb";
-      path = "serial=015ACA59,model=CP2104_USB_to_UART_Bridge_Controller";
+      path = "vendorid=0x10c4,productid=0xea60";
     }
   ];
   microvm.qemu.extraArgs = [
-    "-usb"
     "-device"
-    "/dev/ttyusb0"
+    "qemu-xhci"
+    "-device"
+    "usb-host,vendorid=0x10c4,productid=0xea60"
   ];
-  # microvm.shares = [
-  #   {
-  #     # On the host
-  #     source = "/shared/shares/users/christian/Trading/TWS_Flex_Reports";
-  #     # In the MicroVM
-  #     mountPoint = "/TWS_Flex_Reports";
-  #     tag = "flex";
-  #     proto = "virtiofs";
-  #   }
-  # ];
-
   networking.hostName = hostName;
 
   # |----------------------------------------------------------------------| #
@@ -55,23 +45,6 @@
     inputs.power-meter.packages.${pkgs.system}.power-meter
   ];
   # |----------------------------------------------------------------------| #
-  # systemd.timers."ibkr-flex-download" = {
-  #   wantedBy = ["timers.target"];
-  #   timerConfig = {
-  #     Perisistent = true;
-  #     OnCalendar = "Mon..Fri 23:30";
-  #     Unit = "ibkr-flex-download.service";
-  #   };
-  # };
-
-  # systemd.services."ibkr-flex-download" = {
-  #   serviceConfig = {
-  #     Type = "simple";
-  #     User = "root";
-  #     ExecStart = "${download-ibkr-flex}/bin/ibkr-flex-download";
-  #   };
-  # };
-
   # |----------------------------------------------------------------------| #
   environment.persistence."/persist".files = [
     "/etc/ssh/ssh_host_rsa_key"
