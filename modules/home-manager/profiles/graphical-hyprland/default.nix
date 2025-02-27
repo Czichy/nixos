73417f8d39949c0bf1f32ebaf86983cb3acbe8c4
czichy <christian@czichy.com>
2025-02-27 15:51:03 +0100

    before update

--- modules/home-manager/profiles/graphical-hyprland/default.nix
+++ modules/home-manager/profiles/graphical-hyprland/default.nix
@@ -94,34 +94,34 @@ in {
         noto-fonts-extra
         noto-fonts-cjk-sans
 
-        (pkgs.rustPlatform.buildRustPackage rec {
-          pname = "swayosd";
-          version = "5c2176ae6a01a18fdc2b0f5d5f593737b5765914";
+        # (pkgs.rustPlatform.buildRustPackage rec {
+        #   pname = "swayosd";
+        #   version = "5c2176ae6a01a18fdc2b0f5d5f593737b5765914";
 
-          src = pkgs.fetchFromGitHub {
-            owner = "ErikReider";
-            repo = pname;
-            rev = version;
-            hash = "sha256-rh42J6LWgNPOWYLaIwocU1JtQnA5P1jocN3ywVOfYoc=";
-          };
+        #   src = pkgs.fetchFromGitHub {
+        #     owner = "ErikReider";
+        #     repo = pname;
+        #     rev = version;
+        #     hash = "sha256-rh42J6LWgNPOWYLaIwocU1JtQnA5P1jocN3ywVOfYoc=";
+        #   };
 
-          cargoSha256 = "f/MaNADm/jkEqofd5ixQBcsPr3mjt4qTMRrr0A0J5sI=";
+        #   cargoSha256 = "f/MaNADm/jkEqofd5ixQBcsPr3mjt4qTMRrr0A0J5sI=";
 
-          nativeBuildInputs = with pkgs; [pkg-config];
-          buildInputs = with pkgs; [
-            glib
-            atk
-            gtk3
-            gtk-layer-shell
-            pulseaudio
-          ];
+        #   nativeBuildInputs = with pkgs; [pkg-config];
+        #   buildInputs = with pkgs; [
+        #     glib
+        #     atk
+        #     gtk3
+        #     gtk-layer-shell
+        #     pulseaudio
+        #   ];
 
-          meta = with lib; {
-            description = "A GTK based on screen display for keyboard shortcuts like caps-lock and volume";
-            homepage = "https://github.com/ErikReider/SwayOSD";
-            license = licenses.gpl3;
-          };
-        })
+        #   meta = with lib; {
+        #     description = "A GTK based on screen display for keyboard shortcuts like caps-lock and volume";
+        #     homepage = "https://github.com/ErikReider/SwayOSD";
+        #     license = licenses.gpl3;
+        #   };
+        # })
       ];
 
       fonts.fontconfig.enable = _ true;
