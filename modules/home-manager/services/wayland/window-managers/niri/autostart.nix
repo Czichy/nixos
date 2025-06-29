58d4da8abed39ad3fdf871f3bbaf58e2d6062723
czichy <christian@czichy.com>
2025-06-29 12:09:39 +0200

    niri

---
+++ modules/home-manager/services/wayland/window-managers/niri/autostart.nix
@@ -0,0 +1,20 @@
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  programs.niri.settings.spawn-at-startup = [
+    {command = ["systemctl" "--user" "start" "hyprpolkitagent"];}
+    {command = ["arrpc"];}
+    {command = ["xwayland-satellite"];}
+    {command = ["qs"];}
+    {command = ["hyprlock"];}
+    {command = ["swww-daemon"];}
+    {command = ["swayosd-server"];}
+    {command = ["wl-paste" "--watch" "cliphist" "store"];}
+    {command = ["wl-paste" "--type text" "--watch" "cliphist" "store"];}
+    {command = ["${pkgs.swaynotificationcenter}/bin/swaync"];}
+    {command = ["swayosd --max-volume 150"];}
+    {command = ["xprop -root -f _XWAYLAND_GLOBAL_OUTPUT_SCALE 32c -set _XWAYLAND_GLOBAL_OUTPUT_SCALE 1"];}
+  ];
+}
