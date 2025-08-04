f306b53590375a5d79e236dd6388622362c3ea36
czichy <christian@czichy.com>
2025-08-04 09:43:42 +0200

    ente

--- hosts/HL-1-MRZ-HOST-01/guests/ente.nix
+++ hosts/HL-1-MRZ-HOST-01/guests/ente.nix
@@ -145,6 +145,27 @@ in {
   #   mode = "440";
   #   group = "ente";
   # };
+  age.secrets."rclone.conf" = {
+    file = secretsPath + "/rclone/onedrive_nas/rclone.conf.age";
+    mode = "440";
+    group = "vaultwarden";
+  };
+  age.secrets.restic-postgres = {
+    file = secretsPath + "/hosts/HL-1-MRZ-HOST-01/guests/ente/restic-postgres.age";
+    mode = "440";
+    group = "vaultwarden";
+  };
+
+  age.secrets.ntfy-alert-pass = {
+    file = secretsPath + "/ntfy-sh/alert-pass.age";
+    mode = "440";
+    group = "vaultwarden";
+  };
+
+  age.secrets.postgres-hc-ping = {
+    file = secretsPath + "/hosts/HL-4-PAZ-PROXY-01/healthchecks-ping.age";
+    mode = "440";
+  };
   # |----------------------------------------------------------------------| #
   services.minio = {
     enable = true;
@@ -205,6 +226,89 @@ in {
 
       internal = {
         admin = admin_id;
+      };
+    };
+  };
+
+  # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/nixos/modules/services/backup/restic.nix
+  services.restic.backups = let
+    ntfy_pass = "$(cat ${config.age.secrets.ntfy-alert-pass.path})";
+    ntfy_url = "https://${globals.services.ntfy-sh.domain}/backups";
+    slug = "https://health.czichy.com/ping/";
+
+    script-post = host: site: ''
+      pingKey="$(cat ${config.age.secrets.postgres-hc-ping.path})"
+      if [ $EXIT_STATUS -ne 0 ]; then
+        ${pkgs.curl}/bin/curl -u alert:${ntfy_pass} \
+        -H 'Title: Backup (${site}) on ${host} failed!' \
+        -H 'Tags: backup,restic,${host},${site}' \
+        -d "Restic (${site}) backup error on ${host}!" '${ntfy_url}'
+        ${pkgs.curl}/bin/curl -m 10 --retry 5 --retry-connrefused "${slug}$pingKey/backup-${site}/fail"
+      else
+        ${pkgs.curl}/bin/curl -m 10 --retry 5 --retry-connrefused "${slug}$pingKey/backup-${site}"
+      fi
+    '';
+  in {
+    ente-postgres-backup = {
+      # Initialize the repository if it doesn't exist.
+      initialize = true;
+
+      # backup to a rclone remote
+      repository = "rclone:onedrive_nas:/backup/${config.networking.hostName}-ente-postgres";
+
+      # Which local paths to backup, in addition to ones specified via `dynamicFilesFrom`.
+      paths = ["/tmp/postgresql-dump.sql.gz"];
+
+      # Patterns to exclude when backing up. See
+      #   https://restic.readthedocs.io/en/latest/040_backup.html#excluding-files
+      # for details on syntax.
+      exclude = [];
+
+      passwordFile = config.age.secrets.restic-postgres.path;
+      rcloneConfigFile = config.age.secrets."rclone.conf".path;
+
+      # A script that must run before starting the backup process.
+      backupPrepareCommand =
+        /*
+        sh
+        */
+        ''
+          ${config.services.postgresql.package}/bin/pg_dumpall --clean \
+          | ${lib.getExe pkgs.gzip} --rsyncable \
+          > /tmp/postgresql-dump.sql.gz
+        '';
+
+      # A script that must run after finishing the backup process.
+      backupCleanupCommand =
+        /*
+        sh
+        */
+        ''
+          rm /tmp/postgresql-dump.sql.gz
+        '';
+
+      # Extra extended options to be passed to the restic --option flag.
+      # extraOptions = [];
+
+      # Extra arguments passed to restic backup.
+      # extraBackupArgs = [
+      #   "--exclude-file=/etc/restic/excludes-list"
+      # ];
+
+      # A list of options (--keep-* et al.) for 'restic forget --prune',
+      # to automatically prune old snapshots.
+      # The 'forget' command is run *after* the 'backup' command, so
+      # keep that in mind when constructing the --keep-* options.
+      pruneOpts = [
+        "--keep-daily 3"
+        "--keep-weekly 3"
+        "--keep-monthly 3"
+        "--keep-yearly 3"
+      ];
+
+      # When to run the backup. See {manpage}`systemd.timer(5)` for details.
+      timerConfig = {
+        OnCalendar = "*-*-* 02:30:00";
       };
     };
   };
