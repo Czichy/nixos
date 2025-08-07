59d43c056c5517fb589a2c575675f3712613c538
czichy <christian@czichy.com>
2025-08-07 20:40:04 +0200

    ente

--- hosts/HL-1-MRZ-HOST-02/guests/vaultwarden.nix
+++ hosts/HL-1-MRZ-HOST-02/guests/vaultwarden.nix
@@ -81,6 +81,7 @@ in {
       virtualHosts."${vaultwardenDomain}".extraConfig = ''
         reverse_proxy https://10.15.70.1:443 {
             transport http {
+            tls_insecure_skip_verify
             	tls_server_name ${vaultwardenDomain}
             }
         }
