b86df9b4d9ce32d139f7a4d1259378cbcb4339d8
czichy <christian@czichy.com>
2025-10-12 12:54:52 +0200

    hass

--- hosts/HL-1-MRZ-HOST-03/guests/home-assistant.nix
+++ hosts/HL-1-MRZ-HOST-03/guests/home-assistant.nix
@@ -4,33 +4,87 @@
   lib,
   nodes,
   pkgs,
+  secretsPath,
   ...
 }: let
   homeassistantDomain = "home.${globals.domains.me}";
   certloc = "/var/lib/acme/czichy.com";
 in {
-  imports = [./hass-modbus/mennekes-amtron-xtra.nix];
+  microvm.mem = 4196;
+  microvm.vcpu = 4;
 
-  environment.persistence."/persist".directories = [
-    {
-      directory = config.services.home-assistant.configDir;
-      user = "hass";
-      group = "hass";
-      mode = "0700";
-    }
-  ];
-
+  networking.hostName = "HL-3-RZ-HASS-01";
   globals.services.home-assistant.domain = homeassistantDomain;
   # globals.monitoring.http.homeassistant = {
   #   url = "https://${homeasisstantDomain}";
   #   expectedBodyRegex = "homeassistant";
   #   network = "internet";
   # };
+  #
+  imports = [./hass-modbus/mennekes-amtron-xtra.nix];
 
-  services.matter-server = {
-    enable = true;
-    logLevel = "debug";
+  # |----------------------------------------------------------------------| #
+  networking.firewall = {
+    allowedTCPPorts = [
+      config.services.home-assistant.config.http.server_port
+    ];
   };
+  # Der äußere Caddy (HL-4-PAZ-PROXY-01) muss die Verbindung zum inneren Caddy
+  # über HTTPS aufbauen. Da es sich um eine interne Verbindung handelt und der
+  # innere Caddy möglicherweise ein selbst-signiertes Zertifikat verwendet,
+  # müssen Sie die Zertifikatsprüfung deaktivieren.
+  nodes.HL-4-PAZ-PROXY-01 = {
+    # SSL config and forwarding to local reverse proxy
+    services.caddy = {
+      virtualHosts."${homeassistantDomain}".extraConfig = ''
+        reverse_proxy https://10.15.70.1:443 {
+          transport http{
+            # Da der innere Caddy ein eigenes Zertifikat ausstellt,
+            # muss die Überprüfung auf dem äußeren Caddy übersprungen werden.
+            # tls_insecure_skip_verify
+            tls_server_name ${homeassistantDomain}
+          }
+        }
+        tls ${certloc}/cert.pem ${certloc}/key.pem {
+          protocols tls1.3
+        }
+        import czichy_headers
+      '';
+      # ''
+      #   reverse_proxy https://10.15.70.1:443{
+      #       transport http {
+      #       	tls_server_name ${vaultwardenDomain}
+      #       }
+      #   }
+
+      #   tls ${certloc}/cert.pem ${certloc}/key.pem {
+      #     protocols tls1.3
+      #   }
+      #   import czichy_headers
+      # '';
+    };
+  };
+  # Der innere Caddy (HL-1-MRZ-HOST-02-caddy) muss nun ein eigenes TLS-Zertifikat bereitstellen,
+  # damit der äußere Caddy eine sichere Verbindung aufbauen kann.
+  # Der innere Caddy muss auch seine eigene reverse_proxy-Verbindung zum
+  # Vaultwarden-Server über HTTPS herstellen.
+  nodes.HL-1-MRZ-HOST-02-caddy = {
+    services.caddy = {
+      virtualHosts."${homeassistantDomain}".extraConfig = ''
+        reverse_proxy http://${globals.net.vlan40.hosts."HL-3-RZ-VAULT-01".ipv4}:${toString config.http.server_port}
+        tls ${certloc}/cert.pem ${certloc}/key.pem {
+           protocols tls1.3
+        }
+        import czichy_headers
+      '';
+    };
+  };
+
+  # |----------------------------------------------------------------------| #
+  # services.matter-server = {
+  #   enable = true;
+  #   logLevel = "debug";
+  # };
 
   topology.self.services.home-assistant.info = "https://${homeassistantDomain}";
   services.home-assistant = {
@@ -81,16 +135,20 @@ in {
         server_host = ["0.0.0.0"];
         server_port = 8123;
         use_x_forwarded_for = true;
-        trusted_proxies = [nodes.ward-web-proxy.config.wireguard.proxy-home.ipv4];
+        # trusted_proxies = [nodes.ward-web-proxy.config.wireguard.proxy-home.ipv4];
       };
 
       zha.zigpy_config.source_routing = true;
 
       homeassistant = {
-        name = "!secret ha_name";
-        latitude = "!secret ha_latitude";
-        longitude = "!secret ha_longitude";
-        elevation = "!secret ha_elevation";
+        # name = "!secret ha_name";
+        # latitude = "!secret ha_latitude";
+        # longitude = "!secret ha_longitude";
+        # elevation = "!secret ha_elevation";
+        name = "Czichy Home";
+        latitude = 37;
+        longitude = 37;
+        elevation = 12;
         currency = "EUR";
         time_zone = "Europe/Berlin";
         unit_system = "metric";
@@ -157,10 +215,10 @@ in {
 
     extraPackages = python3Packages:
       with python3Packages; [
-        adguardhome
+        # adguardhome
         aioelectricitymaps
         dwdwfsapi
-        fritzconnection
+        # fritzconnection
         getmac
         gtts
         psycopg2
@@ -172,8 +230,14 @@ in {
   };
 
   age.secrets."home-assistant-secrets.yaml" = {
-    rekeyFile = ./secrets/home-assistant-secrets.yaml.age;
-    owner = "hass";
+    file = secretsPath + "/hosts/HL-1-MRZ-HOST-03/guests/hass/home-assistant-secrets.yaml.age";
+    mode = "440";
+    group = "hass";
+  };
+  age.secrets.influxdb-user-home_assistant-token = {
+    file = secretsPath + "/hosts/HL-1-MRZ-HOST-01/guests/influxdb/home_assistant-token.age";
+    mode = "440";
+    group = "hass";
   };
 
   systemd.services.home-assistant = {
@@ -196,46 +260,55 @@ in {
       touch -a ${config.services.home-assistant.configDir}/{automations,scenes,scripts,manual}.yaml
     '';
   };
+  # |----------------------------------------------------------------------| #
+  environment.persistence."/persist".directories = [
+    {
+      directory = config.services.home-assistant.configDir;
+      user = "hass";
+      group = "hass";
+      mode = "0700";
+    }
+  ];
 
   # Connect to fritzbox via https proxy (to ensure valid cert)
-  networking.hosts.${globals.net.home-lan.vlans.services.hosts.ward-web-proxy.ipv4} = [
-    fritzboxDomain
-  ];
+  # networking.hosts.${globals.net.home-lan.vlans.services.hosts.ward-web-proxy.ipv4} = [
+  #   fritzboxDomain
+  # ];
 
-  networking.hosts.${nodes.ward-adguardhome.config.wireguard.proxy-home.ipv4} = [
-    "adguardhome.internal"
-  ];
+  # networking.hosts.${nodes.ward-adguardhome.config.wireguard.proxy-home.ipv4} = [
+  #   "adguardhome.internal"
+  # ];
 
-  nodes.ward-web-proxy = {
-    services.nginx = {
-      upstreams."home-assistant" = {
-        servers."${config.wireguard.proxy-home.ipv4}:${toString config.services.home-assistant.config.http.server_port}" = {};
-        extraConfig = ''
-          zone home-assistant 64k;
-          keepalive 2;
-        '';
-      };
-      virtualHosts.${homeassistantDomain} = {
-        forceSSL = true;
-        useACMEWildcardHost = true;
-        locations."/" = {
-          proxyPass = "http://home-assistant";
-          proxyWebsockets = true;
-        };
-        extraConfig = ''
-          allow ${globals.net.home-lan.vlans.home.cidrv4};
-          allow ${globals.net.home-lan.vlans.home.cidrv6};
-          allow ${globals.net.home-lan.vlans.devices.cidrv4};
-          allow ${globals.net.home-lan.vlans.devices.cidrv6};
-          # Self-traffic (needed for media in Voice PE)
-          allow ${globals.net.home-lan.vlans.services.hosts.sausebiene.ipv4};
-          allow ${globals.net.home-lan.vlans.services.hosts.sausebiene.ipv6};
-          # Firezone traffic
-          allow ${globals.net.home-lan.vlans.services.hosts.ward.ipv4};
-          allow ${globals.net.home-lan.vlans.services.hosts.ward.ipv6};
-          deny all;
-        '';
-      };
-    };
-  };
+  # nodes.ward-web-proxy = {
+  #   services.nginx = {
+  #     upstreams."home-assistant" = {
+  #       servers."${config.wireguard.proxy-home.ipv4}:${toString config.services.home-assistant.config.http.server_port}" = {};
+  #       extraConfig = ''
+  #         zone home-assistant 64k;
+  #         keepalive 2;
+  #       '';
+  #     };
+  #     virtualHosts.${homeassistantDomain} = {
+  #       forceSSL = true;
+  #       useACMEWildcardHost = true;
+  #       locations."/" = {
+  #         proxyPass = "http://home-assistant";
+  #         proxyWebsockets = true;
+  #       };
+  #       extraConfig = ''
+  #         allow ${globals.net.home-lan.vlans.home.cidrv4};
+  #         allow ${globals.net.home-lan.vlans.home.cidrv6};
+  #         allow ${globals.net.home-lan.vlans.devices.cidrv4};
+  #         allow ${globals.net.home-lan.vlans.devices.cidrv6};
+  #         # Self-traffic (needed for media in Voice PE)
+  #         allow ${globals.net.home-lan.vlans.services.hosts.sausebiene.ipv4};
+  #         allow ${globals.net.home-lan.vlans.services.hosts.sausebiene.ipv6};
+  #         # Firezone traffic
+  #         allow ${globals.net.home-lan.vlans.services.hosts.ward.ipv4};
+  #         allow ${globals.net.home-lan.vlans.services.hosts.ward.ipv6};
+  #         deny all;
+  #       '';
+  #     };
+  #   };
+  # };
 }
