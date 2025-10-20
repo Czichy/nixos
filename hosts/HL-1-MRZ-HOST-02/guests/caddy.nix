e123d897b4d5608c95e13d3cb34352910d9ecb3f
czichy <christian@czichy.com>
2025-10-20 08:43:22 +0200

    caddy

--- hosts/HL-1-MRZ-HOST-02/guests/caddy.nix
+++ hosts/HL-1-MRZ-HOST-02/guests/caddy.nix
@@ -38,9 +38,9 @@ in {
     '';
     globalConfig = ''
       # Caddy-Setup zur automatischen Erstellung und Nutzung einer privaten CA
-      # pki {
-      #     name czichy_internal_ca
-      #   }
+      pki {
+          name czichy_internal_ca
+        }
       servers {
       	trusted_proxies static private_ranges
       	trusted_proxies static 10.46.0.0/24
