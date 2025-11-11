b1beee5355f81548a8985de3c50ced00e9fe3271
czichy <christian@czichy.com>
2025-11-11 20:08:29 +0100

    garage

--- hosts/HL-1-MRZ-HOST-01/guests/s3.nix
+++ hosts/HL-1-MRZ-HOST-01/guests/s3.nix
@@ -56,6 +56,7 @@ in {
   # |----------------------------------------------------------------------| #
   # open firewall ports
   networking.firewall = {
+    enable = true;
     allowedTCPPorts = [9000 3901 3902 3903];
     # allowedUDPPorts = [9000 9001];
   };
