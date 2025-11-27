0c8fe830257d9fef0ec3cbced5ed9f5f4a31d25f
czichy <christian@czichy.com>
2025-11-27 16:42:58 +0100

    grafana

--- modules/home-manager/misc/xdg.nix
+++ modules/home-manager/misc/xdg.nix
@@ -128,6 +128,7 @@ in {
           # Niri
           xdg-desktop-portal-gnome
         ];
+        environment.pathsToLink = ["/share/applications" "/share/xdg-desktop-portal"];
       };
     }
     # |----------------------------------------------------------------------| #
